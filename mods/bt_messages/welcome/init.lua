f1fb57191c661d9430127a92984a5247a04a4fea
Unknown-J <72992404+Unknown-J@users.noreply.github.com>
2020-12-19 18:01:21 +0100

    Update init.lua

--- mods/bt_messages/welcome/init.lua
+++ mods/bt_messages/welcome/init.lua
@@ -1,9 +1,12 @@
 local function show_welcome_message(name)
 	local text = ([[
 						Welcome %s!
-                                                This is the world known as Silver City. It was inspired by talamh
-                                                and beerarchy this is still an anarchy server with a few contributors
-					        and "ALMOST" no moderation besides keeping the server up and running. Feel
+                                                This is the world known as Silver City.
+		                                It was inspired by talamh and beerarchy
+                                                this is still an anarchy server 
+		                                it has a few contributors and "ALMOST" no moderation 
+					        besides keeping the server up and running. 
+		Feel
 						free to say whatever you want in chat. Griefing in any form whatsoever
 						is permitted (destroying structures, lava casting, spawn killing).
 						For those who are easily offended, either mute the chat/player, deal
